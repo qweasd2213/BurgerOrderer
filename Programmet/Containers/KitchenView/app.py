aa4669ae7544d1bd3344df8ecbba3e059b263076
qweasd2213 <qweasd2213@proton.me>
2024-10-11 12:11:23 +0200

    Added suport for quantety and options
    Now kitchenview can handele options and quantety sent from burgerorderer. Also removed the frontpage because kitchenview does not need a web page.

--- Programmet/Containers/KitchenView/app.py
+++ Programmet/Containers/KitchenView/app.py
@@ -3,19 +3,20 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
-def frontpage():
-    return 'Please specify a burger to buy'
-
 @app.route('/buy/<burgerName>', methods=['get'])
 def buy(burgerName):
-    print('One ' + burgerName + ' ordered with the following options:')
-    for arg in request.args:
-        print(' - ' + arg)
-    return "ok"
+    quantity = request.args.get('quantity', '1')
+    options = request.args.getlist('options')
 
+    print(f"Order received: {quantity} {burgerName}")
+    if options:
+        print("options:")
+        for option in options:
+            print(f' - {option}')
+    else:
+        print("No options...")
+    
+    return "Order recived"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-
-
