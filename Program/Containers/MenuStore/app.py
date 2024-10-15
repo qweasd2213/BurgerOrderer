0cfff945856b3cea3c70b16a720ea86d179dc70b
Hugge25 <hugo.paulsson@hotmail.com>
2024-10-15 16:07:36 +0200

    Ändringar i Dockerflie och app.py
    Jag har ändrat lite i filerna och lekt runt lite. Förstår fortfarande inte hur den funkar.

--- Program/Containers/MenuStore/app.py
+++ Program/Containers/MenuStore/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+import psycopg2
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost:5432/your_database_name'
 
+db = SQLAlchemy(app)
+
+connection = psycopg2.connect(host='0.0.0.0', database='asdf', user='adsf', password='adsf')
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=6000)
+
+
