31d1957f47cd12c9d60fe890ba76c0e1fa6450b6
Ante <antelibrary@gmail.com>
2024-10-17 14:58:26 +0200

    Added makeURL tests for each burger and notes in my journal

--- Program/Containers/BurgerOrderer/test_app.py
+++ Program/Containers/BurgerOrderer/test_app.py
@@ -1,4 +1,16 @@
 import app
+"""
+Imports the functions inside of app.py
+"""
 
-def test_make_url():
-    assert app.makeURL("Chicken burger") == "http://localhost:5000/buy/Chicken burger"
+def test_make_url_Halloumi_burger():
+    assert app.makeURL("Halloumi burger") == "http://localhost:5000/buy/Halloumi burger"
+
+def test_make_url_Hamburger():
+    assert app.makeURL("Hamburger") == "http://localhost:5000/buy/Hamburger"
+
+def test_make_url_Deluxe_burger():
+    assert app.makeURL("Deluxe burger") == "http://localhost:5000/buy/Deluxe burger"
+
+def test_make_url_Vegan_burger():
+    assert app.makeURL("Vegan burger") == "http://localhost:5000/buy/Vegan burger"
