8ce4347f3fbc85bff683794e974d867aa60d7d89
Ante <antelibrary@gmail.com>
2024-10-17 15:17:10 +0200

    Revert "Merge branch 'development' of github.com:qweasd2213/BurgerOrderer into development"
    This reverts commit 31a1981a4328a38ee56dff5914185fc11cf3ab3f, reversing
    changes made to 31d1957f47cd12c9d60fe890ba76c0e1fa6450b6.

--- Program/Containers/MenuStore/app.py
+++ Program/Containers/MenuStore/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydb.db' # Specifies what database to use
@@ -21,16 +22,8 @@ class BurgerOrder(Base):
     def __repr__(self): # Makes printing simpler
         return f"({self.burger_id}),{self.burger_name}"
     
-class Option(Base):
-    __tablename__ = "Options"
-    option_id = Column("option_id", Integer, primary_key = True, autoincrement=True)
-    a_option =  Column("a_option", String)
-
-    def __init__(self, option):
-        self.a_option = option
-
-    def __repr__(self):
-        return f"({self.option_id}),{self.a_option}"
+# class Option(Base):
+#     __tablename__ = "options"
 
 
     
@@ -41,40 +34,15 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 session.query(BurgerOrder).delete() #Delets all data in BurgerOrder since otherwise it just adds on and on.
-session.query(Option).delete()
 session.commit()
 
-b1 = BurgerOrder("Chicken burger")
-b2 = BurgerOrder("Halloumi burger")
-b3 = BurgerOrder("Hamburger")
-b4 = BurgerOrder("Deluxe burger")
-b5 = BurgerOrder("Vegan burger")
-
-op1 = Option("3xcheese")
-op2 = Option("Onions")
-op3 = Option("Secret sauce")
-op4 = Option("Tomato")
-op5 = Option("Pickels")
-
+b1 = BurgerOrder("ChickenBurger") 
 session.add(b1)
-session.add(b2)
-session.add(b3)
-session.add(b4)
-session.add(b5)
-
-session.add(op1)
-session.add(op2)
-session.add(op3)
-session.add(op4)
-session.add(op5)
-
 session.commit()
 
-burgers = session.query(BurgerOrder).all() #Queries everything in BurgerOrder and stores in results
-options = session.query(Option).all()
-
-print(burgers)
-print(options)
+results = session.query(BurgerOrder).all() #Queries everything in BurgerOrder and stores in results
+print("RESULTS: ")
+print(results) 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=6000)
